1c65f60881fb670408da79985aff6311ac888db0
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2022-01-23 22:20:52 +0530

    Update Modulo_Multiplicative_Inverse.cpp

--- Modulo_Multiplicative_Inverse.cpp
+++ Modulo_Multiplicative_Inverse.cpp
@@ -1,20 +1,3 @@
-// https://www.codechef.com/LTIME102B/problems/EXPWEI
-#include<bits/stdc++.h>
-#include <stdio.h>
-#include "string.h"
-using namespace std;
-#define int long long 
-#define float double 
-#define pb push_back
-#define all(x) x.begin(),x.end() 
-#define con(s1,s2) strstr(s1.c_str(),s2.c_str())
-#define foreach(x,y) for(auto &x:y)
-#define allr(x) x.rbegin(),x.rend()
-#define sum(a) accumulate(a.begin(), a.end(), (int)0)
-#define ff first
-#define ss second
-#define pi pair<int,int>
-int MOD = 998244353;
 int fmpower(int a,int n) {
     int p=1;
     while(n>0) {
@@ -24,23 +7,4 @@ int fmpower(int a,int n) {
         n=n>>1;
     }
     return p;
-}
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout<<setprecision(0)<<fixed;
-    int t; cin>>t;
-    while(t--) {
-        int n; cin>>n;
-        int ans = (n*(n+1))/2;
-        int fi = n;
-        int x = ans/__gcd(ans,fi);
-        fi = fi/__gcd(fi,ans);
-        int y = ans/__gcd(ans,fi);
-        fi = fi/__gcd(fi,ans);
-        ans = (y%MOD*x%MOD)%MOD;
-        ans = (ans%MOD*fmpower(fi,MOD-2))%MOD;
-        cout<<ans<<endl;
-    }
-    return 0;
 }
