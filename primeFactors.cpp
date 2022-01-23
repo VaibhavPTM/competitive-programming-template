d4daa0097d3c23e60db8a2e99193c15f55cd65c0
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2022-01-23 22:21:18 +0530

    Update primeFactors.cpp

--- primeFactors.cpp
+++ primeFactors.cpp
@@ -1,6 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long int
+
 vector<ll> primeFactors(ll n) {
     vector<ll> p;
     while (n % 2 == 0) {
@@ -16,16 +14,4 @@ vector<ll> primeFactors(ll n) {
     if (n > 2) p.push_back(n);
 //     cout << p.size() << endl;
     return p;
-}
- 
-int main() {
-    ll n;
-    cin >> n;
-    vector<ll> a;
-    primeFactors(n,a);
-    for(int i = 0; i < a.size(); i++) {
-        cout << a[i] << " ";
-    }
-    // cout << (ll)a.size() << endl;
-    return 0;
 }
