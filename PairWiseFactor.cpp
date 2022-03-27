74ec169eed63958661d27ee22e6d3d63197422d7
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2022-03-27 11:24:03 +0530

    Create PairWiseFactor.cpp

---
+++ PairWiseFactor.cpp
@@ -0,0 +1,10 @@
+vec(p(int,int)) factor(int n) {
+    vec(p(int,int)) ar;
+    ar.pb({1, n});
+    for(int i = 2; i * i <= n; i++) {
+        if(n % i == 0) {
+            ar.pb({i, n / i});
+        }
+    }
+    return ar;
+}
