5862439a63d34c786a37e1e61510568bdd715497
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2023-09-28 20:59:57 +0530

    Update PairWiseFactor.cpp

--- PairWiseFactor.cpp
+++ PairWiseFactor.cpp
@@ -1,3 +1,5 @@
+
+// this will give a factor pair wise
 v(p(int,int)) factor(int n) {
     v(p(int,int)) ar;
     ar.pb({1, n});
@@ -7,4 +9,22 @@ v(p(int,int)) factor(int n) {
         }
     }
     return ar;
+}
+
+// this will give all prime factors of numbers
+vector<ll> primeFactors(ll n) {
+    vector<ll> p;
+    while (n % 2 == 0) {
+        p.push_back(2);
+        n = n/2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2) {
+        while (n % i == 0) {
+            p.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2) p.push_back(n);
+//     cout << p.size() << endl;
+    return p;
 }
