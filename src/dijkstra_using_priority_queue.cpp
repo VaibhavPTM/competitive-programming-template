87b44862b8502a0f11c5d21b848a347b58cc694c
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2024-04-11 15:35:36 +0530

    Update dijkstra_using_priority_queue.cpp

--- src/dijkstra_using_priority_queue.cpp
+++ src/dijkstra_using_priority_queue.cpp
@@ -35,3 +35,58 @@ void dijkstra(vector< vector< pair<int,int> > > &G, int vertexCount, int src, in
         }
     }
 }
+vi dijkstra(int sorce, int n = 0)
+{
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    vector<ll> dis(n, 1e18), vis(n);
+    pq.push({0, sorce});
+    dis[sorce] = 0;
+    while (!pq.empty()){
+        pair<ll,ll> cur=pq.top(); 
+        pq.pop(); 
+        if(vis[cur.ss] == 1) continue;
+        for(auto child:g[cur.second]){ 
+            if(cur.first + child.second < dis[child.first]){
+                dis[child.first]=cur.first+child.second;
+                pq.push({dis[child.first], child.first});
+            }
+        }
+        vis[cur.ss] = 1;
+    }
+    return dis;
+}
+void dijkstra(ll n)
+{
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    ll par[n + 1] = {0};
+    vector<ll> dis(n + 1, 1e18), vis(n+1);
+    pq.push({0, 1});
+    dis[1] = 0;
+    par[1] = 1;
+    while (!pq.empty()){
+        pair<ll,ll> cur=pq.top(); 
+        pq.pop(); 
+        if(vis[cur.ss] == 1) continue;
+        for(auto child:graph[cur.second]){ 
+            if(cur.first + child.second < dis[child.first]){
+                dis[child.first]=cur.first+child.second;
+                pq.push({dis[child.first], child.first});
+                par[child.first] = cur.second; 
+            }
+        }
+        vis[cur.ss] = 1;
+    }
+    if (dis[n] >= (ll)1e18 || par[n] == 0)
+       { cout << -1 << endl; return;}
+    vector<ll> ans;
+    while (n != 1)
+    {
+        ans.push_back(n);
+        n = par[n];
+    }
+    ans.push_back(1LL);
+    reverse(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << ' ';
+    cout << '\n';
+}
