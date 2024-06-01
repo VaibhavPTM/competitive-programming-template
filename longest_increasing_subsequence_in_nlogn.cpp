635492040dd9f3f94f3a6d3397d7dfb1593325ec
Vaibhav Patel <76527000+VaibhavPTM@users.noreply.github.com>
2024-06-01 14:47:31 +0530

    Update longest_increasing_subsequence_in_nlogn.cpp

--- longest_increasing_subsequence_in_nlogn.cpp
+++ longest_increasing_subsequence_in_nlogn.cpp
@@ -18,3 +18,34 @@ int lengthOfLIS(vector<int>& v) {
     }
     return ans.size();
 }
+
+// printing LIS
+int findLIS(vector<int> &ar) {
+    int n = ar.size();
+    vector<int> lis;
+    lis.push_back(ar[0]);
+    vector<int> insertedAt(n);
+    insertedAt[0] = 1;
+    for(int i = 1; i < n; i++) {
+        if(ar[i] > lis.back()) {
+            lis.push_back(ar[i]);
+            insertedAt[i] = lis.size();
+        }
+        else {
+            auto it = lower_bound(lis.begin(), lis.end(), ar[i]) - lis.begin();
+            insertedAt[i] = it + 1;
+            lis[it] = ar[i];
+        }
+    }
+    vector<int> res;
+    int len = lis.size();
+    for(int i = n-1; i >= 0; i--) {
+        if(insertedAt[i] == len) {
+            len--;
+            res.push_back(ar[i]);
+        }
+    }
+    reverse(res.begin(), res.end());
+    print(res);
+    return res.size();
+}
